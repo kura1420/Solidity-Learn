0a191c68e2891a285d9d2bf6bb3e5960350d0d83
Abdul Syakur <a.syakur14@gmail.com>
2025-12-08 10:45:30 +0700

    Create voting.sol

---
+++ Belajar/voting.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/*
+Reference: https://www.youtube.com/watch?v=jZJsFjjiDzg&list=PL-buOnh3mkNHKnlojPSZjBWUzHvOlx5vN&index=2
+Rules:
+
+1. Owner: 
+    - Yang deploy SMART CONTRACT
+    - Mendaftarkan kandidat
+    - Mengatur status voting
+
+2. User/Kandidat:
+    - Memilih kandidat
+    - Melihat siapa saja kandidatnya
+    - Melihat siapa yang menang
+*/
+
+contract Evoting {
+
+}
