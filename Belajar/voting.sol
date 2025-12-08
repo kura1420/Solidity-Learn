66a3d5c19466f6a18e6e11a45f2cf08aff4b0211
Abdul Syakur <a.syakur14@gmail.com>
2025-12-08 10:45:51 +0700

    Update voting.sol

--- Belajar/voting.sol
+++ Belajar/voting.sol
@@ -19,4 +19,88 @@ Rules:
 
 contract Evoting {
 
+    address public owner;
+    bool internal isVote;
+    bool internal isDone;
+
+    struct Candidat {
+        address cAddress;
+        uint cValue;
+    }
+    
+    constructor() {
+        owner = msg.sender;
+        isVote = false;
+        isDone = false;
+    }
+
+    Candidat[] public _candidat;
+
+    mapping(address => bool) public candidatRegistered;
+    mapping(address => bool) public userHasVote;
+
+    function addCandidat(address _address) public {
+        require(msg.sender == owner, "Anda bukan owner");
+
+        require(!candidatRegistered[_address], "Data sudah tersedia");
+
+        _candidat.push(
+            Candidat(_address, 0)
+        );
+
+        candidatRegistered[_address] = true;
+    }
+
+    function getTotalCandidat() public view returns (uint) {
+        return _candidat.length;
+    }
+
+    function setVote(bool _value) public {
+        require(msg.sender == owner, "Anda bukan owner");
+
+        isVote = _value;
+    }
+
+    function setDone(bool _value) public {
+        require(msg.sender == owner, "Anda bukan owner");
+
+        isDone = _value;
+    }
+
+    function voting(uint _value) public {
+        require(isVote, "Belum waktunya voting");
+
+        require(_value < _candidat.length, "Kandidat tidak ditemukan");
+
+        require(userHasVote[msg.sender], "Anda sudah voting");
+
+        require(msg.sender != owner, "Owner tidak bisa voting");
+
+        _candidat[_value].cValue++;
+
+        userHasVote[msg.sender] = true;
+    }
+
+    function whoIsWin() public view returns (address, uint) {
+        require(_candidat.length >= 2, "Jumlah kandidat < 2");
+        require(isVote, "Masih proses voting");
+        require(!isDone, "Voting masih berlangsung");
+
+        uint maxValue = 0;
+        uint indexCandidate = 0;
+
+        for (uint i = 0; i < _candidat.length; i++) 
+        {
+            if (_candidat[i].cValue > maxValue) {
+                maxValue = _candidat[i].cValue;
+                indexCandidate = i;
+            } 
+        }
+
+        return (
+            _candidat[indexCandidate].cAddress,
+            _candidat[indexCandidate].cValue
+        );
+    }
+
 }
